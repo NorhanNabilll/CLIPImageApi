ac59b7b1da39b3ee7bc1e8e47d876eb0e2201a0d
norhan-nabil <nabilnorhan324@gmail.com>
2025-06-19 20:02:19 +0300

    fix: Download clip locally

--- ImageSearch.py
+++ ImageSearch.py
@@ -20,7 +20,7 @@ class CLIPImageSearchModel:
     def __init__(self):
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         try:
-            self.model, self.preprocess = clip.load("ViT-B/32", device=self.device)
+            self.model, self.preprocess = clip.load("ViT-B/32", device=self.device, download_root="./clip_models")
             self.model.eval()
             self.initialized = True
         except Exception as e:
